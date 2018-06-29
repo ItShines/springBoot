0cd5eb6a7b449c4c842a5fc69bc72365d638cb36
tranquilityshine@163.com <tranquilityshine@163.com>
2018-06-29 16:04:00 +0800

    springboot 初学 thymeleaf

---
+++ spring_boot/src/main/java/com/liaotianji/Application.java
@@ -0,0 +1,15 @@
+package com.liaotianji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Future
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
