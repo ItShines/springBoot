4032f77da5ee0c5841bba6d360fa81d49875a131
tranquilityshine@163.com <tranquilityshine@163.com>
2018-06-29 17:28:45 +0800

    security 添加 自定义配置

---
+++ readinglist/src/main/java/com/java/service/ReaderRepository.java
@@ -0,0 +1,12 @@
+package com.java.service;
+
+
+import com.java.entity.Reader;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author Future
+ */
+public interface ReaderRepository extends JpaRepository<Reader,String> {
+
+}
